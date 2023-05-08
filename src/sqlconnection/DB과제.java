77363eb673f6c3fea9d50b00a09c1fdbd548ed46
MyStraw <kikongdosa@gmail.com>
2023-05-08 17:50:31 +0900

    5월8일

---
+++ src/sqlconnection/DB과제.java
@@ -0,0 +1,56 @@
+package sqlconnection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class QueryExe{
+	int num;
+	String text;
+	
+	QueryExe(int num, String text){
+		this.num = num;
+		this.text = text;
+	}
+	
+	int getNum() {return num;}
+	String getText() {return text;}	
+}
+
+
+public class DB과제 {
+	
+	public static void exe01 () {
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		ArrayList<QueryExe> list = new ArrayList<>();
+		
+		list.add(new QueryExe(1,"London에 있는 프로젝트 이름을 찾아라."));
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			
+			for (QueryExe qe : list) {
+				System.out.println(String.format("%d. %s", qe.getNum(),qe.getText()));
+			}
+			
+			System.out.print("선택<0:exit>:");
+			int sel = sc.nextInt();
+			if (sel == 0)
+				break;
+			
+			for (QueryExe qe : list) {
+				if(qe.getNum()==sel ) { //스위치 문, else if문, 등등~ //자신있으면 메인 빼고 static 없이 해보세용
+					
+				}
+			}
+		}
+	}
+
+}
---
+++ src/sqlconnection/MillionData_phone_prepared.java
@@ -0,0 +1,82 @@
+package sqlconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
+public class MillionData_phone_prepared {
+	private Connection connectDB() {
+		try {
+			Class.forName("org.h2.Driver");
+			Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/telephone", "scott", "tiger");
+			System.out.println("데이터베이스가 연결되었습니다.");
+			return con;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return null;
+
+	}
+
+	private void closeDB(Connection con) {
+		try {
+			con.close();
+			System.out.println("데이터베이스가 닫혔습니다.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		MillionData_phone_prepared tt = new MillionData_phone_prepared();
+		Connection con = tt.connectDB();
+		if (con != null) {
+			tt.insertContactwithStatement(con);
+			tt.closeDB(con);
+		}
+	}
+
+	private void insertContactwithStatement(Connection con) {
+
+		Statement st = null;
+		String[] types = { "mobile", "home", "office", "fax", "etc" };
+		Random rd = new Random();
+		int totcnt = 1_000_000;
+
+		try {
+			st = con.createStatement();
+
+			for (int i = 1; i <= totcnt; i++) {
+
+				int Seq = rd.nextInt(3) + 1;
+				String type = types[rd.nextInt(5)]; // 0~3
+				String number = String.format("010-%04d-%04d", rd.nextInt(0000, 9999), rd.nextInt(0000, 9999));
+
+				String sql = String.format(
+						"insert into phone (CID, Seq, number, type)" + "values (%d, %d, '%s', '%s')", i, Seq, number,
+						type);
+//				System.out.println(sql);
+				st.executeUpdate(sql);
+
+				System.out.println(String.format("%.2f:%d/%d", i * 100 / (double) totcnt, i, totcnt));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			if (st != null) {
+				try {
+					st.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
