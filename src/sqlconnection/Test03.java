beedf1084055ae502bc873c44d3d9ecc1a83a3c6
MyStraw <kikongdosa@gmail.com>
2023-05-21 21:45:08 +0900

    변경

--- src/sqlconnection/Test03.java
+++ src/sqlconnection/Test03.java
@@ -12,11 +12,7 @@ import java.sql.Statement;
 
 import java.util.Random;
 
-
-
 //contact/phone에 데이터 입력
-
-
 
 /*
 
@@ -42,237 +38,211 @@ PRIMARY KEY (`cid`)
 
 public class Test03 {
 
-
-
 // Statment, executeUpdate
 
-private void insertContactWithStatement(Connection con) {
+	private void insertContactWithStatement(Connection con) {
 
+		Statement st = null;
 
-Statement st = null;
+		String[] cates = { "친구", "가족", "직장동료", "기타" };
 
-String[] cates = {"친구", "가족", "직장동료", "기타"};
+		Random rd = new Random();
 
-Random rd = new Random();
+		int totcnt = 100;
 
-int totcnt = 100;
+		try {
 
+			st = con.createStatement();
 
-try {
+			for (int i = 1; i <= totcnt; i++) {
 
-st = con.createStatement();
+				String name = "name" + i;
 
+				String cate = cates[rd.nextInt(4)];// 0 ~ 3
 
-for (int i = 1 ; i <= totcnt ; i++) {
+				String addr = "addr" + i;
 
+				String company = "company" + i;
 
-String name = "name" + i;
+				String birth = String.format("%4d-%02d-%02d",
 
-String cate = cates[rd.nextInt(4)];// 0 ~ 3
+						rd.nextInt(1950, 2022), rd.nextInt(1, 13), rd.nextInt(1, 29));
 
-String addr = "addr" + i;
+				String sql = String.format("insert into contact (cid, name, cate, addr, company, birth) "
 
-String company = "company" + i;
-
-String birth = String.format("%4d-%02d-%02d",
-
-rd.nextInt(1950, 2022), rd.nextInt(1, 13), rd.nextInt(1, 29));
-
-
-String sql = String.format("insert into contact (cid, name, cate, addr, company, birth) "
-
-+ "values (%d,'%s','%s','%s','%s','%s')", i, name, cate, addr, company, birth);
+						+ "values (%d,'%s','%s','%s','%s','%s')", i, name, cate, addr, company, birth);
 
 //System.out.println(sql);
 
-
 //st.executeUpdate(sql);
 
+				System.out.println(String.format("%.2f:%d/%d", i * 100 / (double) totcnt, i, totcnt));
 
-System.out.println(String.format("%.2f:%d/%d", i*100/(double)totcnt, i, totcnt));
+			}
 
-}
+		} catch (SQLException e) {
 
-} catch (SQLException e) {
+			e.printStackTrace();
 
-e.printStackTrace();
+		} finally {
 
-} finally {
+			if (st != null) {
 
-if (st != null) {
+				try {
 
-try {
+					st.close();
 
-st.close();
+				} catch (SQLException e) {
 
-} catch (SQLException e) {
+					e.printStackTrace();
 
-e.printStackTrace();
+				}
 
-}
+			}
 
-}
+		}
 
-}
+		System.out.println("Contact 입력이 완료되었습니다.");
 
-System.out.println("Contact 입력이 완료되었습니다.");
+	}
 
-}
-
-
-
-/*
-
-CREATE TABLE `phone` (
-
-  `CID` int NOT NULL,
-
-  `Seq` int NOT NULL,
-
-  `number` varchar(20) NOT NULL,
-
-  `type` enum('집','회사','팩스','휴대폰','기타') NOT NULL DEFAULT '기타',
-
-  PRIMARY KEY (`CID`,`Seq`)
-
-)
-
- */
+	/*
+	 * 
+	 * CREATE TABLE `phone` (
+	 * 
+	 * `CID` int NOT NULL,
+	 * 
+	 * `Seq` int NOT NULL,
+	 * 
+	 * `number` varchar(20) NOT NULL,
+	 * 
+	 * `type` enum('집','회사','팩스','휴대폰','기타') NOT NULL DEFAULT '기타',
+	 * 
+	 * PRIMARY KEY (`CID`,`Seq`)
+	 * 
+	 * )
+	 * 
+	 */
 
 // PreaparedStatment, executeUpdate
 
-private void insertPhoneWithPreparedStatement(Connection con) {
+	private void insertPhoneWithPreparedStatement(Connection con) {
 
+		PreparedStatement ps = null;
 
+		String[] cates = { "휴대폰", "집", "회사", "팩스", "기타" };
 
-PreparedStatement ps = null;
+		Random rd = new Random();
 
-String[] cates = {"휴대폰", "집", "회사", "팩스", "기타"};
+		int totcnt = 10000;
 
-Random rd = new Random();
+		String sql = "insert into phone (cid, seq, type, number) values (?, ?, ?, ?)";
 
-int totcnt = 10000;
+		try {
 
+			ps = con.prepareStatement(sql);
 
-String sql = "insert into phone (cid, seq, type, number) values (?, ?, ?, ?)";
+			for (int i = 0; i < totcnt; i++) {
 
+				ps.setInt(1, i / 3 + 1);
 
+				ps.setInt(2, i % 3 + 1);
 
-try {
+				int type = rd.nextInt(5);
 
-ps = con.prepareStatement(sql);
+				ps.setString(3, cates[type]);
 
+				if (type == 0)
 
+					ps.setString(4, String.format("010-%04d-%04d", rd.nextInt(1, 10000), rd.nextInt(1, 10000)));
 
-for (int i = 0 ; i < totcnt ; i++) {
+				else
 
-ps.setInt(1, i/3+1);
-
-ps.setInt(2, i%3+1);
-
-int type = rd.nextInt(5);
-
-ps.setString(3, cates[type]);
-
-if (type == 0)
-
-ps.setString(4, String.format("010-%04d-%04d", rd.nextInt(1, 10000), rd.nextInt(1, 10000)));
-
-else
-
-ps.setString(4, String.format("%03d-%04d-%04d", rd.nextInt(1, 99), rd.nextInt(1, 10000), rd.nextInt(1, 10000)));
-
+					ps.setString(4, String.format("%03d-%04d-%04d", rd.nextInt(1, 99), rd.nextInt(1, 10000),
+							rd.nextInt(1, 10000)));
 
 //ps.executeUpdate();
 
+				System.out.println(String.format("%.2f:%d/%d", (i + 1) * 100 / (double) totcnt, i + 1, totcnt));
 
-System.out.println(String.format("%.2f:%d/%d", (i+1)*100/(double)totcnt, i+1, totcnt));
+			}
 
-}
+		} catch (SQLException e) {
 
-} catch (SQLException e) {
+			e.printStackTrace();
 
-e.printStackTrace();
+		} finally {
 
-} finally {
+			if (ps != null) {
 
-if (ps != null) {
+				try {
 
-try {
+					ps.close();
 
-ps.close();
+				} catch (SQLException e) {
 
-} catch (SQLException e) {
+					e.printStackTrace();
 
-e.printStackTrace();
+				}
 
-}
+			}
 
-}
+		}
 
-}
+		System.out.println("Phone 입력이 완료되었습니다.");
 
-System.out.println("Phone 입력이 완료되었습니다.");
+	}
 
-}
+	private Connection connectDB() {
 
+		try {
 
-private Connection connectDB() {
+			Class.forName("com.mysql.cj.jdbc.Driver");
 
-try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/phonebook", "scott", "tiger");
 
-Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("데이터베이스가 연결되었습니다.");
 
-Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/phonebook", "scott", "tiger");
+			return con;
 
-System.out.println("데이터베이스가 연결되었습니다.");
+		} catch (Exception e) {
 
-return con;
+			e.printStackTrace();
 
-} catch (Exception e) {
+		}
 
-e.printStackTrace();
+		return null;
 
-}
+	}
 
-return null;
+	private void closeDB(Connection con) {
 
-}
+		try {
 
+			con.close();
 
-private void closeDB(Connection con) {
+			System.out.println("데이터베이스가 닫혔습니다.");
 
-try {
+		} catch (Exception e) {
 
-con.close();
+			e.printStackTrace();
 
-System.out.println("데이터베이스가 닫혔습니다.");
+		}
 
-} catch (Exception e) {
+	}
 
-e.printStackTrace();
+	public static void main(String[] args) {
 
-}
+		Test03 tt = new Test03();
 
-}
+		Connection con = tt.connectDB();
 
-public static void main(String[] args) {
+		tt.insertContactWithStatement(con);
 
-Test03 tt = new Test03();
+		tt.insertPhoneWithPreparedStatement(con);
 
+		tt.closeDB(con);
 
-
-Connection con = tt.connectDB();
-
-
-
-tt.insertContactWithStatement(con);
-
-tt.insertPhoneWithPreparedStatement(con);
-
-
-tt.closeDB(con);
-
-}
+	}
 
 }
